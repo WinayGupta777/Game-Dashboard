2e8ec092ae2ffb054d937290350ebb5653480e4a
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-10-10 20:07:54 +0530

    #noissue - Added `alt` property;

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -5,7 +5,7 @@ import worldMap from "E:/MERN/VCode/Game-Dashboard/src/assets/worldmap.png";
 const WorldMap = () => {
   return (
     <div className="worldMap">
-      <img src={worldMap} />
+      <img src={worldMap} alt="World Map" />
     </div>
   );
 };
