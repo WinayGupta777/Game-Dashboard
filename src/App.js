ecfa27a9239c9c71fa1014582a8bf584a9949315
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-10-10 18:56:32 +0530

    Created `AppBar` component; Applied styles;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import AppBarComponent from "./components/AppBar";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="app">
+      <AppBarComponent />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/AppBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/AppBar.css";
+
+const AppBarComponent = () => {
+    return (
+        <div className="appBar">
+            <div className="title">Dashboard</div>
+        </div>
+    );
+};
+
+export default AppBarComponent;
